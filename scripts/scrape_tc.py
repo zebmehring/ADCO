b389f5dd7d0993c9e0b21a67aefd2bf5664cde7b
Zeb Mehring <zeb.mehring@yale.edu>
2018-12-09 17:59:57 -0500

    v0.16.10 -- added a Python script to extract tcs from .tc files

---
+++ scripts/scrape_tc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import re, sys, os
+
+def reduce(tcs):
+    count = 0
+    for line in tcs:
+        m = re.match(r".*\s(\d+)", line)
+        if m != None:
+            count += int(m.group(1))
+    return count
+
+def main():
+    output = "/home/user/Documents/ADCO/results/totals"
+    if os.path.isfile(output):
+        os.remove(output)
+    for file in sys.argv[1:]:
+        if file == output:
+            continue
+        with open(file) as f:
+            tcs = f.readlines()
+            total_count = reduce(tcs)
+        m = re.match(r"(.*\/)*(\w+)\.tc", file)
+        if m != None:
+            with open(output, "a+") as f:
+                f.write(m.group(2) + ": " + str(total_count) + "\n")
+
+if __name__ == "__main__":
+	main()
